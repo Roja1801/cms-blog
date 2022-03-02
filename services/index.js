2c1246dc9e47aeca73028c27d655bc69123e1430
Roja <rojamuthu2001@gmail.com>
2022-03-02 22:39:17 +0530

    fixes

--- services/index.js
+++ services/index.js
@@ -57,7 +57,7 @@ export const getRecentPosts = async () => {
   return result.posts;
 }
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
       posts(
@@ -73,7 +73,7 @@ export const getSimilarPosts = async () => {
       }
     }
   `;
-  const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query,{categories, slug});
 
   return result.posts;
 };
@@ -90,4 +90,40 @@ export const getCategories = async () => {
   const result = await request(graphqlAPI, query);
 
   return result.categories;
+}
+
+export const getPostDetails = async (slug) => {
+  const query = gql`
+  query getPostDetails($slug: String!) {
+    post(where: { slug: $slug })
+          {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+            content {
+              raw
+            }
+          }
+        }
+      `;
+     
+    
+  const result = await request(graphqlAPI,query,{slug});
+  return result.post;
 }
